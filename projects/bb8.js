5a10a426c6eee14056ef9e6b9fb80fbef346d62d
Kent Meyer <kent@softwarecannery.com>
2016-04-17 14:39:30 -0700

    Improvements.

--- projects/bb8.js
+++ projects/bb8.js
@@ -3,51 +3,74 @@ var Cylon = require('cylon');
 Cylon.robot({
   connections: {
     bluetooth: { adaptor: 'central', uuid: 'd16443432e7e', module: 'cylon-ble' },
-    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.108:1883' },
+    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' },
     audio: { adaptor: 'speech' }
   },
 
   devices: {
        bb8: { driver: 'bb8', module: "cylon-sphero-ble", adaptor: 'central', connection: 'bluetooth' },
-   message: { driver: 'mqtt', topic: 'roll', adaptor: 'mqtt', connection: 'server' },
-    voice1: { driver: 'speech', language: "english", gender: "f", speed: 1, connection: 'audio' },
-    voice2: { driver: 'speech', language: "spanish", gender: "m", speed: 120, connection: 'audio' },
-    voice3: { driver: 'speech', language: "spanish", gender: "m", speed: 120, connection: 'audio' },
-    voice4: { driver: 'speech', language: "english", gender: "f", speed: 230, connection: 'audio' }
+   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' },
+    voice1: { driver: 'speech', language: "english", gender: "f", speed: 300, connection: 'audio' },
+    voice2: { driver: 'speech', language: "spanish", gender: "m", speed: 300, connection: 'audio' },
+    voice3: { driver: 'speech', language: "spanish", gender: "m", speed: 300, connection: 'audio' },
+    voice4: { driver: 'speech', language: "english", gender: "f", speed: 300, connection: 'audio' }
   },
 
   work: function(my) {
     my.bb8.color(0x00FFFF);
 
-    my.voice1.say("estoy listo, vaminos!");
     after(500, function() { my.bb8.color(0xFF0000); });
-    after(1000, function() { my.bb8.roll(255, 90); });
-    after(2000, function() { my.bb8.stop(); });
 
  my.message.on('message', function (topic) {
     console.log(topic);
-    if ( topic == "red" ) { 
+    if ( topic == "bb8-red" ) { 
       my.bb8.color(0xFF0000);
-      my.voice1.say("robot es rojo");
     };
-    if ( topic == "green" ) { 
+    if ( topic == "bb8-green" ) { 
       my.bb8.color(0x00FF00);
-      my.voice2.say("robot es verde");
     };
-    if ( topic == "blue" ) { 
+    if ( topic == "bb8-blue" ) { 
       my.bb8.color(0x0000FF);
-      my.voice3.say("robot es azul");
+    };
+    if ( topic == "bb8-spin-left" ) { 
+      my.bb8.spin("right",150);
+    };
+    if ( topic == "bb8-spin-right" ) { 
+      my.bb8.spin("left",150);
+    };
+    if ( topic == "bb8-spin-stop" ) { 
+      my.bb8.spin("right",0);
     };
     if ( topic == "roll-short" ) { 
-      my.bb8.roll(255, 90);
-      my.voice1.say("rolling");
+      my.bb8.roll(100, 0);
+      setTimeout(function() { my.bb8.stop(0); }, 1000);
+    };
+    if ( topic == "bb8-roll-up" ) { 
+      my.bb8.roll(60, 0);
       setTimeout(function() { my.bb8.stop(0); }, 2000);
     };
-    if ( topic == "azul" ) { 
-      my.bb8.color(0xF0FFFF);
-      my.voice4.say("The robot is some sort of lightblueish greenish color. Why do I talk so fast? Change this garrett. Pleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeease. Help! Help! Help! Help me garrett! Please!");
-      after(20000, function() { my.voice1.say("No."); });
-      
+    if ( topic == "bb8-roll-right" ) { 
+      my.bb8.roll(60, 90);
+      setTimeout(function() { my.bb8.stop(0); }, 2000);
+    };
+    if ( topic == "bb8-roll-back" ) { 
+      my.bb8.roll(60, 180);
+      setTimeout(function() { my.bb8.stop(0); }, 2000);
+    };
+    if ( topic == "bb8-roll-left" ) { 
+      my.bb8.roll(60, 270);
+      setTimeout(function() { my.bb8.stop(0); }, 2000);
+    };
+    if ( topic == "bb8-no" ) { 
+      my.bb8.spin("right",120);
+      setTimeout(function() { my.bb8.spin("left",120); }, 200);
+      setTimeout(function() { my.bb8.spin("right",120); }, 400);
+      setTimeout(function() { my.bb8.spin("left",120); }, 600);
+      setTimeout(function() { my.bb8.spin("right",0); }, 800);
+    };
+    if ( topic == "bb8-yes" ) { 
+      setTimeout(function() { my.bb8.roll(120,0); }, 200);
+      setTimeout(function() { my.bb8.stop(0); }, 600);
     };
 
  });
--- projects/cheeky.js
+++ projects/cheeky.js
@@ -3,30 +3,27 @@ var DCD = require('dream-cheeky-thunder-driver');
 
 Cylon.robot({
   connections: {
-    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.108:1883' },
+    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' },
+    audio: { adaptor: 'speech' }
   },
 
   devices: {
-   message: { driver: 'mqtt', topic: 'roll', adaptor: 'mqtt', connection: 'server' },
+   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' },
+    voice1: { driver: 'speech', language: "english", gender: "f", speed: 200, connection: 'audio' },
   },
 
   work: function(my) {
 
+    my.voice1.say("initiating missiles");
     my.message.on('message', function (topic) {
     console.log(topic);
 
-    if ( topic == "red" ) { };
-    if ( topic == "green" ) { };
-    if ( topic == "blue" ) { };
-    if ( topic == "roll-short" ) { };
-    if ( topic == "azul" ) { };
-    if ( topic == "ev3-move" ) { };
-    if ( topic == "ardrone" ) { };
     if ( topic == "cheeky-up" ) { DCD.moveUp(500); };
     if ( topic == "cheeky-down" ) { DCD.moveDown(500); };
     if ( topic == "cheeky-right" ) { DCD.moveRight(500); };
     if ( topic == "cheeky-left" ) { DCD.moveLeft(500); };
     if ( topic == "cheeky-fire" ) { DCD.fire(1); };
+    if ( topic == "cheeky-park" ) { DCD.park(); };
  });
   }
 }).start();
