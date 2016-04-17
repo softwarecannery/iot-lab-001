c6491da6a7c20af7afcaa1f78aaed2e590d08fb1
Kent Meyer <kent@softwarecannery.com>
2016-04-17 15:12:11 -0700

    Name those light bulbs!

--- projects/lights.js
+++ projects/lights.js
@@ -1,3 +1,5 @@
+// To run:
+// $ node examples/blink/blink.js 192.168.1.85 35dacee025cd94cf3f50bb301ad8b4bf
 
 "use strict";
 
@@ -6,23 +8,22 @@ var Cylon = require("cylon");
 Cylon.robot({
   connections: { 
     hue: { adaptor: "hue", host: process.argv[2], username: process.argv[3] },
-    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' },
-    audio: { adaptor: 'speech' }
+    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' }
   },
 
   devices: {
-   bulb: { driver: "hue-light", lightId: 1 },
-   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' },
-    voice1: { driver: 'speech', language: "english", gender: "f", speed: 200, connection: 'audio' },
+   bulbJeffery: { driver: "hue-light", lightId: 1 },
+   bulbFrancis: { driver: "hue-light", lightId: 2 },
+   bulbPiper: { driver: "hue-light", lightId: 3 },
+   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' }
   },
 
   work: function(my) {
-    my.voice1.say("initiating lights");
     my.message.on('message', function (topic) {
       console.log(topic);
-      if ( topic == "red" ) { my.bulb.rgb(255,0,0); };
-      if ( topic == "blue" ) { my.bulb.rgb(0,0,255); };
-      if ( topic == "green" ) { my.bulb.rgb(0,255,0); };
+      if ( topic == "red" ) { my.bulbJeffery.rgb(255,0,0); };
+      if ( topic == "blue" ) { my.bulbFrancis.rgb(0,0,255); };
+      if ( topic == "green" ) { my.bulbPiper.rgb(0,255,0); };
 	
 	
     });
