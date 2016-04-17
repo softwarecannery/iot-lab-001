b29632fa9a977bf4e23ddf6bac3fd012bc8948f4
Kent Meyer <kent@softwarecannery.com>
2016-04-17 15:08:56 -0700

    check in

--- projects/lights.js
+++ projects/lights.js
@@ -1,5 +1,3 @@
-// To run:
-// $ node examples/blink/blink.js 192.168.1.85 35dacee025cd94cf3f50bb301ad8b4bf
 
 "use strict";
 
@@ -8,15 +6,18 @@ var Cylon = require("cylon");
 Cylon.robot({
   connections: { 
     hue: { adaptor: "hue", host: process.argv[2], username: process.argv[3] },
-    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' }
+    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' },
+    audio: { adaptor: 'speech' }
   },
 
   devices: {
    bulb: { driver: "hue-light", lightId: 1 },
-   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' }
+   message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' },
+    voice1: { driver: 'speech', language: "english", gender: "f", speed: 200, connection: 'audio' },
   },
 
   work: function(my) {
+    my.voice1.say("initiating lights");
     my.message.on('message', function (topic) {
       console.log(topic);
       if ( topic == "red" ) { my.bulb.rgb(255,0,0); };
