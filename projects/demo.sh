849ab5226da5e737f9fb3490f6e24a68c9e299e9
Kent Meyer <kent@softwarecannery.com>
2016-04-17 14:24:04 -0700

    more fine-tuning.

---
+++ projects/demo.sh
@@ -0,0 +1,14 @@
+:
+mosquitto_pub -h 192.168.168.105 -t iot red
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot blue
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot green
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot cheeky-up
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot cheeky-down
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot cheeky-left
+sleep 1
+mosquitto_pub -h 192.168.168.105 -t iot cheeky-fire
