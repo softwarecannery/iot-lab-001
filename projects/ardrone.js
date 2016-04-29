08631fd9bb38990062774a91c1925c138af49153
Kent Meyer <kent@softwarecannery.com>
2016-04-29 10:09:39 -0700

    latest ardrone

--- projects/ardrone.js
+++ projects/ardrone.js
@@ -1,27 +1,50 @@
-
 var Cylon = require('cylon');
 
 Cylon.robot({
   connections: {
-    ardrone: { adaptor: 'ardrone', port: '192.168.168.168' }
+    ardrone: { adaptor: 'ardrone', port: '192.168.1.1' },
+    server: { adaptor: 'mqtt', host: 'mqtt://192.168.168.105:1883' }
   },
 
   devices: {
-    drone: { driver: 'ardrone' }
+    drone: { driver: 'ardrone' },
+    message: { driver: 'mqtt', topic: 'iot', adaptor: 'mqtt', connection: 'server' }
   },
 
   work: function(my) {
-    my.drone.takeoff();
-    after((4).seconds(), function() {my.drone.up(1);});
-    after((5).seconds(), function() {my.drone.hover();});
-    after((6).seconds(), function() {my.drone.frontFlip(1);});
-    after((8).seconds(), function() {my.drone.hover(1);});
-    after((10).seconds(), function() {my.drone.down(1);});
-    after((12).seconds(), function() {my.drone.hover();});
-    after((14).seconds(), function() {my.drone.left(1);});
-    after((15).seconds(), function() {my.drone.right(1);});
-    after((17).seconds(), function() {my.drone.hover();});
-    after((18).seconds(), function() { my.drone.land(); });
-    after((22).seconds(), function() { my.drone.stop(); });
+    my.message.on('message', function (topic) {
+
+    console.log(topic);
+    if ( topic == "ardrone-takeoff" ) { 
+      my.drone.takeoff();
+      console.log("takeoff");
+    };
+if ( topic == "ardrone-up" ) { 
+      my.drone.up(1);
+      console.log("UP");
+    };
+
+if ( topic == "ardrone-hvr" ) { 
+      my.drone.hover();
+      console.log("UP");
+    };
+
+if ( topic == "ardrone-left" ) { 
+      my.drone.left(1);
+      console.log("UP");
+    };
+
+if ( topic == "ardrone-right" ) { 
+      my.drone.right(1);
+      console.log("UP");
+    };
+
+if ( topic == "ardrone-stop" ) { 
+      my.drone.stop();
+      my.drone.land();
+      console.log("STOPPING");
+    };
+   
+    });
   }
 }).start();
